1cd460258cbc9f15015b9bf7095e3203e76e7c23
Paul Young <paulyoungonline@gmail.com>
2014-09-02 22:45:22 -0400

    Initial Commit

---
+++ MessageTransfer/MessageTransfer.h
@@ -0,0 +1,19 @@
+//
+//  MessageTransfer.h
+//  MessageTransfer
+//
+//  Created by Paul Young on 02/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MessageTransfer.
+FOUNDATION_EXPORT double MessageTransferVersionNumber;
+
+//! Project version string for MessageTransfer.
+FOUNDATION_EXPORT const unsigned char MessageTransferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageTransfer/PublicHeader.h>
+
+
