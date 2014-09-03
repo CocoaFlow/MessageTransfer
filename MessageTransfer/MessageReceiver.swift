5b4948defd57f4aab4aae2487e6a543420aa7a7a
Paul Young <paulyoungonline@gmail.com>
2014-09-02 23:09:06 -0400

    Add protocols for sending and receiving messages.

---
+++ MessageTransfer/MessageReceiver.swift
@@ -0,0 +1,18 @@
+//
+//  MessageReceiver.swift
+//  MessageTransfer
+//
+//  Created by Paul Young on 02/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+import Foundation
+import JSONLib
+
+public protocol MessageReceiver {
+    func receive(channel: String, _ topic: String, _ payload: JSON)
+}
+
+public protocol MessageReceiverWorkaround: MessageReceiver {
+    var messageSender: MessageSender? { get set }
+}
---
+++ MessageTransfer/MessageSender.swift
@@ -0,0 +1,18 @@
+//
+//  MessageSender.swift
+//  MessageTransfer
+//
+//  Created by Paul Young on 02/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+import Foundation
+import JSONLib
+
+public protocol MessageSender {
+    func send(channel: String, _ topic: String, _ payload: JSON)
+}
+
+public protocol MessageSenderWorkaround: MessageSender {
+    var messageReceiver: MessageReceiver? { get set }
+}
